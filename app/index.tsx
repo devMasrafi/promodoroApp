f901f80f5672b12b19fcf02807eccf44f353135b
Md. Masrafi Mondol <masrafisw@gmail.com>
2025-01-20 22:18:14 +0600

    setup & nativewind config

---
+++ app/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View>
+      <Text className="text-2xl font-bold" >welcome</Text>
+    </View>
+  );
+}
